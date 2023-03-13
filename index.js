76838fa804af13d0f09685f2709d5aa3165163a8
JollyR0ger <kav993@gmail.com>
2023-03-13 15:09:14 +0200

    refactoring

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 8080
 
+app.use(express.static('root'))
 app.use(express.static('public'))
 
 app.listen(port)
---
+++ root/_nuxt/composables.2c8b7c4d.js
@@ -0,0 +1,1 @@
+import{k as r}from"./entry.9971d2ff.js";function t(e,u){return r()._useHead(e,u)}export{t as u};
