7498ceaecfdefcb2408d567c0507680e1b894a23
JollyR0ger <kav993@gmail.com>
2023-03-22 14:26:05 +0200

    add test-getwin

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 8080
 
 app.use(express.static('root'))
 app.use(express.static('public'))
+app.use(express.static('test-getwin'))
 
 app.listen(port)
 console.log('Server started at http://localhost:' + port)
