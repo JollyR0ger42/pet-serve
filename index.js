546e764ad7b1be005d87981b58ce39c77ce64c0c
JollyR0ger <kav993@gmail.com>
2023-08-16 21:13:28 +0300

    add ContactUs page

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const port = process.env.PORT || 8080;
 app.use(compression())
 app.use(express.static('root'));
 app.use(express.static('public'));
-app.use(express.static('test-getwin'));
 
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
