f6b72db725bd6eaf036b556448902250ce4f02d3
SnakeCheck <kav993.@gmail.com>
2024-06-24 16:58:11 +0300

    fix mb

--- index.js
+++ index.js
@@ -8,5 +8,9 @@ app.use(compression())
 app.use(express.static('root'));
 app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'root', 'index.html'));
+});
+
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
