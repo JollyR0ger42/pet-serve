494921aa777381a91903888d6dcdf9c9533b389f
SnakeCheck <kav993.@gmail.com>
2024-06-24 17:36:41 +0300

    fix?

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(compression())
-app.use('/', express.static('root'));
-app.use('/', express.static('public'));
+app.use('/root', express.static('root'));
+app.use('/public', express.static('public'));
+
+app.get("/", (req, res) => {
+    res.send("Express on Vercel");
+  });
 
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
